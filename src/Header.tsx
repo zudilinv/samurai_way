13986d9e9307dbb31ed48cc4aa1e2bd22623afff
zudilinv <102663010+zudilinv@users.noreply.github.com>
2023-10-16 16:08:17 +0400

    Second commit

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div>
+            <a href={"#s"}>Home</a>
+            <a href={"#s"}>Messages</a>
+            <a href={"#s"}>News Feed</a>
+        </div>
+    )
+}
---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+            </ul>
+        </div>
+    )
+}
